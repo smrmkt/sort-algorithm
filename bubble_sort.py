6971fd9118840b12be4c772fb304c54c6be40954
smrmkt <smrmkt@gmail.com>
2015-12-25 21:33:32 +0900

    separate bin file and algorithm

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,34 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import argparse
-import random
-import time
 
-
-def init_array(n):
-    return [random.randrange(n*10) for i in range(n)]
-
-
-def bubble_sort(a):
-    for i in range(len(a)-1, 0, -1):
-        for j in range(i):
-            if a[j] > a[j+1]:
-                a[j], a[j+1] = a[j+1], a[j]
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-n', type=int, nargs='?', default=20)
-    parser.add_argument('-d', type=bool, nargs='?', default=0)
-    n = parser.parse_args().n
-    debug = parser.parse_args().d
-
-    a = init_array(n)
-    if debug:
-        print a
-    start = time.time()
-    bubble_sort(a)
-    if debug:
-        print a
-    erapsed = time.time() - start
-    print erapsed
+class BubbleSort():
+    
+    @staticmethod
+    def execute(a):
+        for i in range(len(a)-1, 0, -1):
+            for j in range(i):
+                if a[j] > a[j+1]:
+                    a[j], a[j+1] = a[j+1], a[j]
+        return a
---
+++ execute_sort.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import random
+import time
+
+from bubble_sort import BubbleSort
+
+
+sorts = {
+    'bubble': BubbleSort
+}
+
+
+def init_array(n):
+    return [random.randrange(n*10) for i in range(n)]
+
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', type=str, nargs='?', default='bubble',
+                        choices=['bubble', 'insert', 'quick'])
+    parser.add_argument('-d',action='store_true', default=False)
+    parser.add_argument('-n', type=int, nargs='?', default=20)
+    n = parser.parse_args().n
+    debug = parser.parse_args().d
+    algorithm = parser.parse_args().a
+
+    # execute sort    
+    before = init_array(n)
+    if debug:
+        print before
+    start = time.time()
+    sort = sorts[algorithm]
+    after = sort.execute(before)
+    if debug:
+        print after
+    erapsed = time.time() - start
+    print erapsed

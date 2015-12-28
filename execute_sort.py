b0c23b0a79854910582b39153335e81d4d6d2a89
smrmkt <smrmkt@gmail.com>
2015-12-28 09:43:49 +0900

    add target array ordered mode

--- execute_sort.py
+++ execute_sort.py
@@ -18,7 +18,9 @@ sorts = {
 }
 
 
-def init_array(n):
+def init_array(n, is_ordered):
+    if is_ordered:
+        return [i for i in range(n)]
     return [random.randrange(n*10) for i in range(n)]
 
 
@@ -29,18 +31,20 @@ if __name__ == '__main__':
                         choices=['bubble', 'insert', 'quick', 'squick'])
     parser.add_argument('-d',action='store_true', default=False)
     parser.add_argument('-n', type=int, nargs='?', default=20)
+    parser.add_argument('-o',action='store_true', default=False)
     n = parser.parse_args().n
-    debug = parser.parse_args().d
+    is_debug = parser.parse_args().d
     algorithm = parser.parse_args().a
+    is_ordered = parser.parse_args().o
 
     # execute sort    
-    before = init_array(n)
-    if debug:
+    before = init_array(n, is_ordered)
+    if is_debug:
         print before
     start = time.time()
     sort = sorts[algorithm]
     after = sort(before).execute()
-    if debug:
+    if is_debug:
         print after
     erapsed = time.time() - start
     print erapsed
